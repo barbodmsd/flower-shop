f5bcb0b527e8b3af5f2eccc05bf3867260c59874
Barbodmsd <barbodmasoudi@gmail.com>
2024-06-25 13:28:24 +0430

    end navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import { Box, ThemeProvider, createTheme } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products";
+import ProductDetails from "./Pages/ProductDetails";
+import Cart from "./Pages/Cart";
+import Auth from "./Pages/Auth";
+import NotFound from "./Pages/NotFound";
 
+const theme = createTheme({
+  palette: {
+    txt: "#811414",
+    bg: "#EAB9B7",
+    bglight:'#f3d6d5'
+  },
+});
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <Box minHeight={"80vh"}>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
+          <Route
+            path='/product-details/:id/:name'
+            element={<ProductDetails />}
+          />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/auth' element={<Auth />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Box>
+
+      <Footer />
+    </ThemeProvider>
+  );
 }
---
+++ client/src/Components/Navbar/index.jsx
@@ -0,0 +1,110 @@
+import { Button, IconButton, Input, InputAdornment, Stack } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Slide from "@mui/material/Slide";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import PropTypes from "prop-types";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import logo from "./../../../public/assets/logo.png";
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction='down' in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function Navbar(props) {
+  return (
+    <>
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <AppBar sx={{ bgcolor: "transparent" }}>
+          <Stack
+            direction={"row"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            sx={{ height: "70px", px: "50px", bgcolor: "bglight" }}>
+            {/* left side */}
+            <Stack direction={"row"} alignItems={"center"} gap={"12px"}>
+              <Typography>
+                <img
+                  style={{
+                    mixBlendMode: "darken",
+                  }}
+                  src={logo}
+                  width={"120px"}
+                  height={"50px"}
+                  alt={"logo"}
+                />
+              </Typography>
+              <Link to={"/"}>
+                <Button size={"large"} sx={{ color: "txt", bgcolor: "bg" }}>
+                  Home
+                </Button>
+              </Link>
+            </Stack>
+            {/* right side */}
+            <Stack direction={"row"} alignItems={"center"} gap={"12px"}>
+                {/* search */}
+              <Box
+                width={"170px"}
+                sx={{
+                  bgcolor: "bg",
+                  borderRadius: "5px",
+                }}>
+                <Input
+                  endAdornment={
+                    <InputAdornment position='end'><SearchRoundedIcon sx={{color:'txt'}}/></InputAdornment>
+                  }
+                  fullWidth
+                  disableUnderline={true}
+                  sx={{
+                    px: " 10px ",
+                  }}
+                />
+              </Box>
+              {/* cart */}
+             <Link to={'/cart'}><IconButton>
+                <ShoppingBasketIcon sx={{
+                    color:'txt'
+                }}/>
+             </IconButton></Link>
+              {/* auth */}
+             <Link to={'/auth'}><IconButton>
+                <PersonRoundedIcon sx={{
+                    color:'txt'
+                }}/>
+             </IconButton></Link>
+            </Stack>
+          </Stack>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
+    </>
+  );
+}
